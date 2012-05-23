b02ae1a1f66f2b73358353d9bcf0c90b5cad1d35
CowanSM <cowansm@gmail.com>
2012-05-23 15:13:51 -0400

    google auth fix

--- larva_library/oauth.py
+++ larva_library/oauth.py
@@ -357,7 +357,7 @@ function.
         url = add_query(self.expand_url(self.access_token_url), remote_args)
         resp, content = self._client.request(url, self.access_token_method)
         data = parse_response(resp, content)
-        if resp['status'] != '200' & resp['status'] != '405':
+        if resp['status'] != '200':
             print 'status was not 200, instead:'
             print vars(resp)
             raise OAuthException('Invalid response from ' + self.name, data)
