e349ddefa72d7a8c1deb734284b68ebb18d1d96a
CowanSM <cowansm@gmail.com>
2012-05-23 09:49:47 -0400

    google attempt 2

--- larva_library/__init__.py
+++ larva_library/__init__.py
@@ -25,11 +25,14 @@ twitter = oauth.remote_app('twitter',
     consumer_key=app.config.get('TWITTER_CONSUMER_KEY'),
     consumer_secret=app.config.get('TWITTER_CONSUMER_SECRET')
 )
-google=oauth.remote_app('google',
+google = oauth.remote_app('google',
     base_url='https://accounts.google.com/',
     request_token_url='https://accounts.google.com/o/oauth2/auth',
     access_token_url='https://www.googleapis.com/oauth2/v1/tokeninfo',
-    authorize_url='https://accounts.google.com/o/oauth2/auth')
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    consumer_key=app.config.get('GOOGLE_CLIENT_ID'),
+    consumer_secret=app.config.get('GOOGLE_CLIENT_SECRET')
+)
 
 # Create connection to Mongo
 connection = Connection(host=app.config.get('MONGODB_HOST'),
