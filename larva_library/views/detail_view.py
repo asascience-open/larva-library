106dba43eee2962f731f640c5bd0cb966b6821c2
Sean Cowan <scowan@asascience.com>
2012-05-31 13:33:31 -0400

    detail view now follows GET req

--- larva_library/views/detail_view.py
+++ larva_library/views/detail_view.py
@@ -1,13 +1,14 @@
-from flask import redirect, url_for, render_template, flash, session
+from flask import redirect, url_for, render_template, flash, session, request
 from larva_library import app, db
 
-@app.route('/detail/<library_entry>')
-def detail_view(library_entry):
-	if library_entry is None:
+@app.route('/detail')
+def detail_view():
+	entry_name = request.args.get('entry_name')
+	if entry_name is None:
 		flash('Cannot find an entry without a name')
 		return redirect(url_for('index'))
 	else:
-	    entry = db.Library.find_one({'User':session['user_email'],'Name':library_entry})
+	    entry = db.Library.find_one({'User':session['user_email'],'Name':entry_name})
 	    if entry is None:
 	        flash('Cannot find ' + library_name + ' for user ' + session['user_email'])
 	        return redirect(url_for('index'))
