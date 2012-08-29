c4f20ef57c1e3c5ed4e069ade5b09c5a51239fc5
Kyle Wilcox <kwilcox@asascience.com>
2012-08-29 17:30:56 -0400

    Add crossdomain.xml

--- larva_library/views/index.py
+++ larva_library/views/index.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, session, flash, request
+from flask import render_template, redirect, url_for, session, flash, request, make_response
 from larva_library import app, db
 from larva_library.models.library import LibrarySearch
 
@@ -12,3 +12,16 @@ def index():
         libraries = list(db.Library.find({'user':user}))
     
     return render_template('index.html', libraries=libraries, form=form)
+
+@app.route('/crossdomain.xml', methods=['GET'])
+def crossdomain():
+    domain = """
+    <cross-domain-policy>
+        <allow-access-from domain="*"/>
+        <site-control permitted-cross-domain-policies="all"/>
+        <allow-http-request-headers-from domain="*" headers="*"/>
+    </cross-domain-policy>
+    """
+    response = make_response(domain)
+    response.headers["Content-type"] = "text/xml"
+    return response
